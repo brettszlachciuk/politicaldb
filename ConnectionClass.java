f75edf328fe7f6f20c17876f19c8834c56e28c5a
bszlachciuk <brettszlachciuk@gmail.com>
2019-10-24 01:02:23 -0400

    Add files via upload

---
+++ ConnectionClass.java
@@ -0,0 +1,38 @@
+package connectivity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Scanner;
+
+public class ConnectionClass {
+  Scanner scanner = new Scanner(System.in);
+  
+  public Connection connection;
+
+  private final String serverName = "localhost";
+
+  private final int portNumber = 3306;
+  
+  private final String dbName = "politicaldb";
+  
+  private String userName = "root";
+  
+  private String password = "password";
+  
+  
+  private String url = "jdbc:mysql://"
+      + this.serverName + ":" + this.portNumber + "/" + this.dbName;
+  
+
+  
+  public Connection getConnection() {
+    try {
+    connection = DriverManager.getConnection(this.url, this.userName, this.password);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    return connection;
+  }
+
+}
